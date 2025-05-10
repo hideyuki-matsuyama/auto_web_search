d6d14a64bf6ccf49cd978155eaeed6c629a9d96f
Hideyuki Matsuyama <choripon@gmail.com>
2025-05-11 04:39:55 +0900

    WIP: duckduckgo.com対応

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 gem install selenium-webdriver
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --disable-gcm --user-data-dir=/tmp/chrome_dev
 time ruby main.rb
+
+15min : 300
+x : 2000
+
+300x : 30000
+x = 100min
+
+matsuyamahideyuki@Mac-Studio company_search % time ruby main.rb
+検索結果が 検索結果.txt に保存されました。
+ruby main.rb 10.05s user 5.61s system 0% cpu 1:18:08.94 total
--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
 require 'selenium-webdriver'
 require 'clipboard'
+require 'uri'
 
 # 既存の Chrome に接続
 options = Selenium::WebDriver::Chrome::Options.new
 options.debugger_address = 'localhost:9222'
 options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)')
 driver = Selenium::WebDriver.for(:chrome, options: options)
+
+# ページロード待機
+driver.manage.timeouts.implicit_wait = 1.5
 
 # 企業名リストのファイルパス
 input_file_path = '企業名リスト.txt'
@@ -14,42 +18,23 @@ output_file_path = '検索結果.txt'
 # 企業名リストを読み込む
 lines = File.readlines(input_file_path).map(&:strip)
 
-lines.each_slice(25) do |sub_lines|
-  results = sub_lines.map do |line|
-    # ロボット認定対策
-    sleep rand 5..10
+File.open(output_file_path, 'a') do |file|
+  lines.each_with_index do |line, idx|
+    # 検索
+    search_url = 'https://duckduckgo.com?' + URI.encode_www_form([['t', 'h_'], ['q', "\"#{line}\" 企業ページ"], ['ia', 'web']])
+    # search_url = 'https://duckduckgo.com?' + URI.encode_www_form([['t', 'h_'], ['q', "#{line} 企業ページ"], ['ia', 'web']])
+    driver.navigate.to search_url
 
-    # Googleで検索
-    driver.navigate.to('https://www.google.com')
-
-    # sleep 1  # ページロード待機
-    driver.manage.timeouts.implicit_wait = 10
-
-    search_box = driver.find_element(name: 'q')
-    search_box.send_keys("#{line} 企業ページ")
-    search_box.submit
-
-    # sleep 1  # ページロード待機
-    driver.manage.timeouts.implicit_wait = 10
-
-    # 検索結果のURL取得（最初の結果）
-    # 法人格が含まれないことがあるため機械的に最初を見る
-    first_result = driver.find_elements(xpath: '//h3[1]/parent::a')[0]
+    # 検索結果のURL取得
+    # SERP に法人格が含まれないことがあるため機械的に最初を見る
+    first_result = driver.find_elements(xpath: '//h2/a')[0]
     result = first_result ? first_result.attribute('href') : '🔥'
 
-    "#{line}\t#{result}"
-  end
+    # ウェイトをランダムに設定
+    sleep(rand 3..6) if idx % 10 == 0
 
-  # 検索結果を保存するファイルを開く
-  File.open(output_file_path, 'a') do |file|
-    results.each do |result|
-      # 別ファイルに企業名と検索結果URLを保存
-      file.puts result
-    end
+    file.puts "#{line}\t#{result}"
   end
-
-  # ロボット認定対策
-  # sleep rand 10..15
 end
 
 puts "検索結果が #{output_file_path} に保存されました。"
